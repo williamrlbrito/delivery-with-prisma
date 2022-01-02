9b367a3fac0f92df828ad8d3ce779e197cb99eeb
William Brito <williamrldb@gmail.com>
2022-01-02 07:14:40 -0400

    --Criar autenticação do deliveryman

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,35 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prismaClient";
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+  async execute({ username, password }: IAuthenticateDeliveryman) {
+    const deliveryman = await prisma.deliveryman.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!deliveryman) {
+      throw new Error("Username or password is incorrect");
+    }
+
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if (!passwordMatch) {
+      throw new Error("Username or password is incorrect");
+    }
+
+    const token = sign({ username }, "822569f5c6edc28140895fabdeba477d", {
+      subject: deliveryman.id,
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+}
