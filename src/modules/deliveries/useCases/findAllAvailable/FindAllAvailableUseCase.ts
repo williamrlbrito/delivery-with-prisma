de051d359c476234d4fd4b489214fe6bea2ff9e8
William Brito <williamrldb@gmail.com>
2022-01-02 11:57:56 -0400

    --Update de entrega adicionando entregador

--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -3,7 +3,10 @@ import { prisma } from "../../../../database/prismaClient";
 export class FindAllAvailableUseCase {
   async execute() {
     const deliveries = await prisma.deliveries.findMany({
-      where: { end_at: null },
+      where: {
+        end_at: null,
+        id_deliveryman: null,
+      },
     });
     return deliveries;
   }
