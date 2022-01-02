4fcf18ee6f412c3462fb783630a090e409b7840c
William Brito <williamrldb@gmail.com>
2022-01-02 09:14:33 -0400

    --Middleware de deliveryman autenticado

---
+++ src/middlewares/ensureAuthenticateDeliveryman.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticateDeliveryman(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ message: "Token not provided" });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub } = verify(
+      token,
+      "822569f5c6edc28140895fabdeba477d"
+    ) as IPayload;
+
+    request.id_deliveryman = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ message: "Token invalid" });
+  }
+}
