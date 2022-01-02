acefd0544c8283826142e4e18fefe70334289cbc
William Brito <williamrldb@gmail.com>
2022-01-01 20:48:47 -0400

    --Cadastro de cliente

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,34 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    return client;
+  }
+}
